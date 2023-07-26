3fbf0cd114fdcbe0d2828c3916cec7f0ba26e0ea
roodhouse <rughjm@gmail.com>
2023-07-26 16:38:31 -0500

    top copy button components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import Top from './components/Top'
+import Copy from './components/Copy'
+import Button from './components/Button'
+
+// setup popup as final component
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="App h-screen" id='app'>
+     <div id='mainWrapper' className='h-full bg-[url("../src/assets/mobile/bg-image-daytime.jpg")] bg-no-repeat bg-contain'>
+      <div id='mainContainer' className='h-full pt-8 px-[26px] pb-10 bg-black bg-opacity-40'>
+        <div id='topWrapper'>
+          <Top />
+        </div>
+        <div id='copyWrapper' className='mt-[227px] mb-12'>
+          <Copy />
+        </div>
+        <div id='buttonWrapper'>
+          <Button />
+        </div>
+      </div>
+
+     </div>
     </div>
   );
 }
